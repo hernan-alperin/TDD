d796c15880d2fe2caf1095f50393c2cb290e4d56
Hernán Alperin <hernan.alperin@gmail.com>
2019-12-17 13:28:08 -0300

    set my daat

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 
@@ -12,13 +10,12 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='segmemtador en TDD',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Test Designed Dev del segmantador para el censo202',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Hernán Alperin',
+    author_email='alpe@alum.mit.edu',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
